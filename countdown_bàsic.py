e723ed7819080ae354d40ae9750e6f39109871f4
jcanudas <106311027+jcanudas@users.noreply.github.com>
2022-06-01 13:35:45 +0200

    Add files via upload

---
+++ countdown_bàsic.py
@@ -0,0 +1,11 @@
+# Programa countdown bàsic
+# Li donem un valor determinat a la variable i
+# Pots canviar el valor i posar el que vulguis
+# El programa farà un compte enrera des del valor i fins a zero
+
+i = 10
+
+while i > 0:
+    print(str(i))
+    i -= 1
+
